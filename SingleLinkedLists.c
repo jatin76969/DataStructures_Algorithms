6c79d3a8e04b3c5ac5e21a3ea3d742d83be3c707
jatin76969 <anusharani.kotha12@gmail.com>
2026-02-06 15:32:54 +0530

    Update SingleLinkedLists.c

--- SingleLinkedLists.c
+++ SingleLinkedLists.c
@@ -72,3 +72,127 @@ void deleteNode(struct LinkedList* list, int position) {
      
      
      if(position < 0 || position >= list -> size)
+	{
+		printf("Invalid position\n");
+		return;
+	}
+	struct Node* temp = list -> head;
+	if(position == 0)
+	{
+		list -> head = temp -> next;
+		free(temp);
+	}
+	else{
+		struct Node* prev = NULL;
+		for(int i = 0; i < position; i++)
+			{
+				prev = temp;
+				temp = temp -> next;
+			}
+		prev -> next = temp -> next;
+		free(temp);
+	}
+	list -> size --;
+	printf("Element at position %d deleted\n", position);
+    
+    
+    
+    
+    
+    
+    
+    
+}
+void reverse(struct LinkedList* list) {
+     // write your code here 
+     struct Node* prev = NULL;
+	struct Node* current = list->head;
+	struct Node* next = NULL;
+	while (current != NULL) 
+
+		{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	list -> head = prev;
+    
+    
+    
+    
+    
+    
+}
+void display(struct LinkedList* list) {
+    struct Node* current = list->head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+void displayReversed(struct LinkedList* list) {
+    struct Node* current = list->head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    int num_elements;
+    printf("Enter the number of elements in the linked list: ");
+    scanf("%d", &num_elements);
+    struct LinkedList* list = initializeList();
+    int data;
+    printf("Enter the elements:\n");
+    for (int i = 0; i < num_elements; i++) {
+        scanf("%d", &data);
+        insert(list, data, i);
+    }
+    int choice, position;
+    while (1) {
+        printf("Linked List Operations:\n");
+        printf("1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Reverse\n");
+        printf("4. Display\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("Enter data to insert: ");
+                scanf("%d", &data);
+                printf("Enter position to insert: ");
+                scanf("%d", &position);
+                insert(list, data, position);
+                break;
+            case 2:
+                printf("Enter position to delete: ");
+                scanf("%d", &position);
+                deleteNode(list, position);
+                break;
+            case 3:
+                reverse(list);
+                printf("Reversed List: ");
+                displayReversed(list);
+                reverse(list); 
+                break;
+            case 4:
+                printf("Linked List: ");
+                display(list);
+                break;
+            case 5:
+                printf("Exiting program\n");
+                exit(0);
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+
+    return 0;
+}
